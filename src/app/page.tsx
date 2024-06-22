2302928ae85ddf5de8d987198d248ec0986b4244
Danny Lucas <lannyducas@gmail.com>
2024-06-22 15:54:52 -0400

    added flex box to homepage css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className = "bg-blue-100">
+    <div className = "flex flex-col min-h-screen bg-blue-100">
       <Head>
         <title>My Landing Page</title>
         <meta name="description" content="Welcome to my landing page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="flex-grow">
         <h1 className="text-primary-900 text-3xl text-center font-display">Welcome to Danny's blog!</h1>
         <p className="text-primary-800 font-body">This is where my blog posts will be.</p>
       </main>
 
-      <footer>
-        <p className="text-primary-600 font-body text-bottom">© 2024 My Website</p>
+      <footer className="bg-gray-800 bg-opacity-80 p-4 text-primary-800 text-center">
+        <p>© 2024 My Website</p>
       </footer>
     </div>
   );
